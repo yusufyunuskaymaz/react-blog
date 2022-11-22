930bdaacafab750e61df3463d276d2b7b6196cbe
yusufKaymaz <kaymazyusuf88@gmail.com>
2022-11-22 09:28:53 +0300

    deleted old

--- old/src/index.js
+++
@@ -1,6 +0,0 @@
-import App from './App';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
--- old/src/pages/Settings/Settings.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Settings = () => {
-  return (
-    <div><h1>Settings</h1></div>
-  )
-}
-
-export default Settings
